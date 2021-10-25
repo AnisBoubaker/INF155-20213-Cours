ee16102d9384ec676a12115156bd47466e2fe699
Anis Boubaker <anis@boubaker.ca>
2021-10-25 14:55:23 -0400

    Gr1 - Tri

---
+++ Gr01/INF155-1-C8/Tri/tri.c
@@ -0,0 +1,41 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE_MAX 100
+
+void tri_insertion(int tab[], int taille);
+
+int main(void)
+{
+	int tab[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35 };
+
+	tri_insertion(tab, 7);
+
+	for (int i = 0; i < 7; i++)
+	{
+		printf("%d\t", tab[i]);
+	}
+	printf("\n");
+
+	system("pause");
+	return 0;
+}
+
+void tri_insertion(int tab[], int taille)
+{
+	int x; 
+	int j;
+	for (int i = 1; i < taille; i++)
+	{
+		x = tab[i];
+		j = i;
+		while (j > 0 && tab[j - 1] > x)
+		{
+			tab[j] = tab[j - 1];
+			j--;
+		}
+		tab[j] = x;
+	}
+}
